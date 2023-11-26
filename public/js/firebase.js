cd7830dca3036dd8dd06b3a2b2e90f3a65e2ce8b
Siddharth Sadhan <142924404+WolfsBane-ops@users.noreply.github.com>
2023-11-26 11:13:00 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,11 @@
 let firebaseConfig = {
-    // Enter your firebase credentials
+    apiKey: "AIzaSyC7iw154e2HE7yfRASGFaq9jKPjU6fmv94",
+    authDomain: "vercelli-eabd6.firebaseapp.com",
+    projectId: "vercelli-eabd6",
+    storageBucket: "vercelli-eabd6.appspot.com",
+    messagingSenderId: "921199222810",
+    appId: "1:921199222810:web:133d35e5b85f44773d31a3",
+    measurementId: "G-Q4JYRDWZ2N"
 };
 
 firebase.initializeApp(firebaseConfig);
